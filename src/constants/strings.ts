1dab6bf876e1ca9cb79a8e0de3d0889cd25989cb
Patrick Watts <redbeardtn@yahoo.com>
2022-02-12 20:33:49 -0600

    Testing attributes

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,6 +1,6 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
+export const WIN_MESSAGES = ['Your motion is carried!', 'Celebrate with a potluck!', 'Well done!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const ABOUT_GAME_MESSAGE = 'About this game'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
