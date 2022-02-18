aab76d0e4e6b1100ddad37e2e5a343abb52dd185
Patrick Watts <redbeardtn@yahoo.com>
2022-02-17 22:04:26 -0600

    updated app name, tweaked formatting of about

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,7 +9,7 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-<a href="https://sbcthisweek.com" className="underline font-bold" target="_blank" rel="noreferrer">Brought to you by the fine folks at SBC This Week</a><br ></br><br></br>
+<a href="https://sbcthisweek.com" className="underline font-bold" target="_blank" rel="noreferrer">Brought to you by the fine folks at <br></br>SBC This Week</a><br ></br><br></br>
 
         Based on an open source version of the word guessing game we all know and
         love -{' '}
