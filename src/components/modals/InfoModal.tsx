de82d054df7db363258c559d8a13736e314ee7a6
Patrick Watts <redbeardtn@yahoo.com>
2022-02-18 18:59:04 -0600

    fixed styling error

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -13,8 +13,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         This is a <em>special</em> adaptation of the famous word guessing game designed to get you ready and excited each day 
         before the <a href="https://sbcannualmeeting.net" className="underline font-bold" target="_blank" rel="noreferrer">SBC 2022 Annual Meeting in Anaheim</a>. All of the words in this game are related to the categories of 
         church culture, SBC polity and entities, Bible books, annual meeting lingo, or California tourism.</p>
-        <hr style="margin: 10px 0px;"></hr>
-        <p className="text-sm text-gray-500 dark:text-gray-300" style="margin-top: 20px;">
+        <hr style={{ margin: '10px 0px' }}></hr>
+        <p className="text-sm text-gray-500 dark:text-gray-300">
         Guess the word in 6 tries. After each guess, the color of the tiles will
         change to show how close your guess was to the word.
       </p>
