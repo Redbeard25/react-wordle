7f2423db4c682678f36965a6e2f3dae34ec75635
Patrick Watts <redbeardtn@yahoo.com>
2022-02-18 18:52:26 -0600

    added info to the infobox on word content, link to AM

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,10 +10,11 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-700 dark:text-gray-100">
-        This is a <em>special</em> adaptation of the famous word guessing game designed to get you ready and excited about 
-        the run-up to the SBC 2022 Annual Meeting in Anaheim. All of the words in this game are related to the categories of 
-        church culture, SBC polity, annual meeting lingo, or California tourism.</p>
-        <p className="text-sm text-gray-500 dark:text-gray-300">
+        This is a <em>special</em> adaptation of the famous word guessing game designed to get you ready and excited each day 
+        before the <a href="https://sbcannualmeeting.net" className="underline font-bold" target="_blank" rel="noreferrer">SBC 2022 Annual Meeting in Anaheim</a>. All of the words in this game are related to the categories of 
+        church culture, SBC polity and entities, Bible books, annual meeting lingo, or California tourism.</p>
+        <hr style="margin: 10px 0px;"></hr>
+        <p className="text-sm text-gray-500 dark:text-gray-300" style="margin-top: 20px;">
         Guess the word in 6 tries. After each guess, the color of the tiles will
         change to show how close your guess was to the word.
       </p>
